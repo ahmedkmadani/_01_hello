17318840aabfbcb3128b6251d03639d5864b61b2
Ahmed K Madani <ahmedkamal543@gmail.com>
2022-03-07 00:43:39 +0300

    working with range for map and slice

--- _09_range/main.go
+++ _09_range/main.go
@@ -3,5 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+
+	ids := []int{33, 45, 24, 45, 0}
+
+	//Loop through ids using range
+
+	for _, id := range ids {
+		fmt.Printf("ID %d\n", id)
+	}
+
+	//Add ids together
+	sum := 0
+	for _, id := range ids {
+		sum += id
+	}
+	fmt.Println("Sum", sum)
+
+	//Range with MAP
+
+	emails := map[string]string{"Boob": "boob@mail.com", "Mike": "mike@mail.com"}
+
+	for k, v := range emails {
+		fmt.Printf("%s is name - %s is email \n", k, v)
+	}
+
+	for k := range emails {
+		fmt.Printf("%s name", k)
+	}
+
 }
